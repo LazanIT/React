0ee75d34f6a885be5f62174b2dd25dba5cb1ab5d
LazanIT <117667390+LazanIT@users.noreply.github.com>
2024-02-18 21:51:21 +0100

    First part finish
    The finish of first part introduction into React, and starting with jsx

--- adobt-me/src/App.js
+++ adobt-me/src/App.js
@@ -1,9 +1,30 @@
+const Pet = (props) => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, props.name),
+    React.createElement("h2", {}, props.animal),
+    React.createElement("h2", {}, props.breed),
+  ]);
+};
+
 const App = () => {
-  return React.createElement(
-    "div",
-    {},
-    React.createElement("h1", {}, "Adopt Me!")
-  );
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Adopt Me!"),
+    React.createElement(Pet, {
+      animal: "Dog",
+      name: "Luna",
+      breed: "Havanese",
+    }),
+    React.createElement(Pet, {
+      animal: "Bird",
+      name: "Paper",
+      breed: "Cocktiel",
+    }),
+    React.createElement(Pet, {
+      animal: "Cat",
+      name: "Doink",
+      breed: "Mixed",
+    }),
+  ]);
 };
 
 const container = document.getElementById("root");
