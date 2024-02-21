c918a58fc83bab8adae89ed20558ef717e8de5b5
LazanIT <117667390+LazanIT@users.noreply.github.com>
2024-02-22 00:27:25 +0100

    React Hook Exercise
    Animal array, exercise with Array Map

--- adobt-me/src/SearchParams.jsx
+++ adobt-me/src/SearchParams.jsx
@@ -1,13 +1,27 @@
 import { useState } from 'react';
 
+const Animals = ["Bird", "Cat","Dog", "Rabbit", "Reptile"];
+
 const SearchParams = () => {
    const [location, setLocation] = useState(""); 
+   const [animal, setAnimal] =useState("");
     return (
         <div className="search-params">
             <form>
                 <label htmlFor="location">
                     location
                     <input onChange={e => setLocation(e.target.value)} id="location" value={location} placeholder="location" />
+                </label>
+                <label htmlFor="animal">
+                    animal
+                    <select id="animal" value={animal} onChange={e => {
+                        setAnimal(e.target.value)
+                    }}>
+                        <option />
+                        {Animals.map((animal) => (
+                            <option key={animal}>{animal}</option>
+                        ))}
+                    </select>
                 </label>
                 <button type="button">Submit</button>
             </form>
