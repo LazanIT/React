ef5f819b75c93268a2d64af940991eaafb2dba01
Aleksandar Lazanski <aleksandar.lazanski3@gmail.com>
2024-04-12 00:50:52 +0200

    Tailwind CSS start

--- adobt-me/src/App.jsx
+++ adobt-me/src/App.jsx
@@ -1,13 +1,10 @@
 import { useState } from "react";
 import { createRoot } from "react-dom/client";
-import { Link, BrowserRouter, Routes, Route } from "react-router-dom"
-import { QueryClient, QueryClientProvider, } from "@tanstack/react-query"
+import { Link, BrowserRouter, Routes, Route } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import AdoptedPetContext from "./AdoptedPetContext";
 import SearchParams from "./SearchParams";
 import Details from "./Details";
-
-
-
 
 // const App = () => {
 //   return React.createElement("div", {}, [
@@ -34,31 +31,37 @@ const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
       staleTime: Infinity,
-      cacheTime: Infinity
-    }
-  }
-})
-
+      cacheTime: Infinity,
+    },
+  },
+});
 
 const App = () => {
   const adoptedPet = useState(null);
-  return(
-    <BrowserRouter>
-    <QueryClientProvider client={queryClient}>
-        <AdoptedPetContext.Provider value={adoptedPet}>
-          <header>
-            <Link to="/">Adopt me!</Link>
-          </header>
-      <Routes>
-          <Route path="/details/:id" element={<Details/>}></Route>
-          <Route path="/" element={<SearchParams/>}></Route>
-      </Routes>
-      </AdoptedPetContext.Provider>
-      </QueryClientProvider>
-  </BrowserRouter>
-  )
-}
+  return (
+    <div
+      className="m-0 p-0"
+      style={{
+        background: "url(http://pets-images.dev-apis.com/pets/wallpaperA.jpg)",
+      }}
+    >
+      <BrowserRouter>
+        <QueryClientProvider client={queryClient}>
+          <AdoptedPetContext.Provider value={adoptedPet}>
+            <header>
+              <Link to="/">Adopt me!</Link>
+            </header>
+            <Routes>
+              <Route path="/details/:id" element={<Details />}></Route>
+              <Route path="/" element={<SearchParams />}></Route>
+            </Routes>
+          </AdoptedPetContext.Provider>
+        </QueryClientProvider>
+      </BrowserRouter>
+    </div>
+  );
+};
 
 const container = document.getElementById("root");
 const root = createRoot(container);
-root.render(<App />)
+root.render(<App />);
