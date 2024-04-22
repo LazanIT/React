6b4195a9b70cbb0cf2fe27c856a234783b35db62
Aleksandar Lazanski <aleksandar.lazanski3@gmail.com>
2024-04-22 23:30:09 +0200

    APIResponse and Pet Shape
    Throwing new Error and Pet shape ( what kind of data it will be,  for example id, name, animal. desc... and other things

--- server-side-rendering/src/APIResponsesTypes.ts
+++ server-side-rendering/src/APIResponsesTypes.ts
@@ -8,3 +8,24 @@
 */
 
 export type Animal = "dog" | "cat" | "bird" | "reptile" | "rabbit";
+
+// Kako ce nasa zivotinja da izgleda
+// PET SHAPE
+export interface Pet {
+  id: number;
+  name: string;
+  animal: Animal;
+  description: string;
+  breed: string;
+  images: string[];
+  city: string;
+  state: string;
+}
+
+export interface PetAPIResponse {
+  numberOfResults: number;
+  startIndex: number;
+  endIndex: number;
+  hasNext: boolean;
+  pets: Pet[];
+}
