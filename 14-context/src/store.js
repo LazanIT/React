c547d497276002033143707e7be132f36ed46bde
Aleksandar Lazanski <aleksandar.lazanski3@gmail.com>
2024-04-28 23:52:59 +0200

    Redux Toolkit Query and Middleware

--- 14-context/src/store.js
+++ 14-context/src/store.js
@@ -1,12 +1,16 @@
 import { configureStore } from "@reduxjs/toolkit";
 import adoptedPet from "./adoptedPetSlice";
 import searchParams from "./searchParamsSlice";
+import { petApi } from "./petApiService";
 
 const store = configureStore({
   reducer: {
     adoptedPet,
     searchParams,
+    [petApi.reducerPath]: petApi.reducer,
   },
+  middleware: (curryGetDefaultMiddleware) =>
+    curryGetDefaultMiddleware().concat(petApi.middleware),
 });
 
 export default store;
